1606a47fd5ad1e02efc807c437029d5c8d999fe8
Laura202301 <fedele.laura17@gmail.com>
2023-07-13 16:05:02 -0300

    Implement dynamic grid size adjustment through slider

--- javascript.js
+++ javascript.js
@@ -1,22 +1,49 @@
 // Get the grid container element
 const gridContainer = document.querySelector('.grid-container');
+const slider = document.getElementById('my-range');
+const scale = document.getElementById('scale');
 
-// Create the grid items
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement('div');
-  gridItem.classList.add('grid-item');
-  gridContainer.appendChild(gridItem);
+// Function to update the grid size
+function updateGridSize() {
+  const gridSize = parseInt(slider.value);
+  const gridItems = Array.from(gridContainer.getElementsByClassName('grid-item'));
 
-  // Set initial background color
-  gridItem.style.backgroundColor = '#ccc';
-
-  // Add event listeners to each grid item
-  gridItem.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'red';
+  // Remove existing grid items
+  gridItems.forEach((item) => {
+    gridContainer.removeChild(item);
   });
 
-  gridItem.addEventListener('mouseout', function() {
-    this.style.backgroundColor = 'red';
-  });
+  // Create new grid items with the updated size
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+
+    // Set initial background color
+    gridItem.style.backgroundColor = '#ccc';
+
+    // Set the new flex-basis and padding-top values
+    const flexBasis = `calc(${100 / gridSize}% - 2px)`;
+    const paddingTop = `calc(${100 / gridSize}% - 2px)`;
+    gridItem.style.flexBasis = flexBasis;
+    gridItem.style.paddingTop = paddingTop;
+
+    // Add event listeners to each grid item
+    gridItem.addEventListener('mouseover', function() {
+      this.style.backgroundColor = 'red';
+    });
+
+    gridItem.addEventListener('mouseout', function() {
+      this.style.backgroundColor = 'red';
+    });
+  }
+
+  // Update the scale display
+  scale.textContent = `${gridSize} x ${gridSize}`;
 }
 
+// Add event listener to the slider input
+slider.addEventListener('input', updateGridSize);
+
+// Initialize the grid size
+updateGridSize();
