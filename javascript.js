b8afbaadff5a3c3b8e0cc915fd1c69ff71ffdc56
Laura202301 <fedele.laura17@gmail.com>
2023-07-19 17:41:15 -0300

    Implement dot buttons (black, multicolor, clear) functionality

--- javascript.js
+++ javascript.js
@@ -2,12 +2,16 @@
 const gridContainer = document.querySelector('.grid-container');
 const slider = document.getElementById('my-range');
 const scale = document.getElementById('scale');
-const colorPicker = document.getElementById('fav-color'); 
+const colorPicker = document.getElementById('fav-color');
+const blackDot = document.querySelector('.black-dot'); 
+const multicolorDot = document.querySelector('.multicolor-dot'); 
+const clearDot = document.querySelector('.clear-dot'); 
 
-let currentColor = colorPicker.value; 
+let currentColor = colorPicker.value;
+let isMulticolorMode = false; 
 
 // Function to update the grid size
-function updateGridSize() {
+function updateGridSize() { 
   const gridSize = parseInt(slider.value);
   const gridItems = Array.from(gridContainer.getElementsByClassName('grid-item'));
 
@@ -31,13 +35,20 @@ function updateGridSize() {
     gridItem.style.flexBasis = flexBasis;
     gridItem.style.paddingTop = paddingTop;
 
-    // Add event listeners to each grid item
+    // Add event listeners to each grid item 
     gridItem.addEventListener('mouseover', function() {
-      this.style.backgroundColor = currentColor; 
+      if (isMulticolorMode) {
+        const randomColor = getRandomColor();
+        this.style.backgroundColor = randomColor;
+      } else {
+        this.style.backgroundColor = currentColor;
+      }
     });
 
     gridItem.addEventListener('mouseout', function() {
-      this.style.backgroundColor = currentColor; 
+      if (!isMulticolorMode) {
+        this.style.backgroundColor = currentColor;
+      }
     });
   }
 
@@ -45,14 +56,42 @@ function updateGridSize() {
   scale.textContent = `${gridSize} x ${gridSize}`;
 }
 
-// Function to handle color picker change
-function handleColorChange() { 
-  currentColor = colorPicker.value; 
+function handleColorChange() {
+  currentColor = colorPicker.value;
+  isMulticolorMode = false; 
+}
+
+// Functions to handle dot buttons 
+function handleBlackDotClick() { 
+  isMulticolorMode = false;
+  currentColor = '#000000';
+}
+
+function handleMulticolorDotClick() { 
+  isMulticolorMode = true;
+}
+
+function handleClearDotClick() { 
+  isMulticolorMode = false;
+  currentColor = '#ccc';
+}
+
+// Helper function to generate a random color 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 // Add event listeners
 slider.addEventListener('input', updateGridSize);
-colorPicker.addEventListener('input', handleColorChange); 
+colorPicker.addEventListener('input', handleColorChange);
+blackDot.addEventListener('click', handleBlackDotClick); 
+multicolorDot.addEventListener('click', handleMulticolorDotClick); 
+clearDot.addEventListener('click', handleClearDotClick); 
 
 // Initialize the grid size
 updateGridSize();
