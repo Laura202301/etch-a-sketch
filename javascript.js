100ab809ad567eb0abdff02dbee37e8c4eb3453d
Laura202301 <fedele.laura17@gmail.com>
2023-07-25 22:11:36 -0300

    Implement darkening effect toggle switch functionality

--- javascript.js
+++ javascript.js
@@ -7,9 +7,12 @@ const blackDot = document.querySelector('.black-dot');
 const multicolorDot = document.querySelector('.multicolor-dot'); 
 const clearDot = document.querySelector('.clear-dot'); 
 const resetButton = document.querySelector('.reset-button');
+const darkeningSwitch = document.querySelector('.darkening-switch input');
 
 let currentColor = colorPicker.value;
-let isMulticolorMode = false; 
+let isMulticolorMode = false;
+let isDarkeningEnabled = false; // Variable to enable/disable darkening
+const maxInteractions = 10; // Check if the max number of interactions has been reached
 
 // Function to update the grid size
 function updateGridSize() { 
@@ -36,18 +39,48 @@ function updateGridSize() {
     gridItem.style.flexBasis = flexBasis;
     gridItem.style.paddingTop = paddingTop;
 
-    // Add event listeners to each grid item 
+    // Add event listeners to each grid item
     gridItem.addEventListener('mouseover', function() {
-      if (isMulticolorMode) {
-        const randomColor = getRandomColor();
-        this.style.backgroundColor = randomColor;
+      if (isDarkeningEnabled) {
+        const currentColor = this.style.backgroundColor;
+        
+        // Check if the current color is '#ccc' (grid color)
+        if (currentColor !== 'rgb(204, 204, 204)') {
+          const currentColorRGB = currentColor.match(/\d+/g);
+          let [r, g, b] = currentColorRGB.map(Number);
+          
+          // Calculate the value to darken the current color
+          const darkeningValue = Math.floor(255 / maxInteractions);
+          
+          // Check if the maximum number of interactions has been reached
+          const totalInteractions = parseInt(this.getAttribute('data-interactions')) || 0;
+          if (totalInteractions < maxInteractions) {
+            r = Math.max(0, r - darkeningValue);
+            g = Math.max(0, g - darkeningValue);
+            b = Math.max(0, b - darkeningValue);
+            
+            // Increase the interaction counter
+            this.setAttribute('data-interactions', totalInteractions + 1);
+          }
+          
+          // Update background color
+          this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
       } else {
-        this.style.backgroundColor = currentColor;
+        // If darkening is not enabled, color with the current color
+        if (isMulticolorMode) {
+          const randomColor = getRandomColor();
+          this.style.backgroundColor = randomColor;
+        } else {
+          this.style.backgroundColor = currentColor;
+        }
       }
     });
 
     gridItem.addEventListener('mouseout', function() {
-      if (!isMulticolorMode) {
+      // Reset the interaction counter when leaving the square
+      this.setAttribute('data-interactions', 0);
+      if (!isDarkeningEnabled && !isMulticolorMode) {
         this.style.backgroundColor = currentColor;
       }
     });
@@ -77,7 +110,7 @@ function handleClearDotClick() {
   currentColor = '#ccc';
 }
 
-// Function to reset the grid item colors  //NUEVO
+// Function to reset the grid item colors
 function resetGridColors() {
   const gridItems = Array.from(gridContainer.getElementsByClassName('grid-item'));
   gridItems.forEach((item) => {
@@ -102,6 +135,9 @@ blackDot.addEventListener('click', handleBlackDotClick);
 multicolorDot.addEventListener('click', handleMulticolorDotClick); 
 clearDot.addEventListener('click', handleClearDotClick); 
 resetButton.addEventListener('click', resetGridColors); 
+darkeningSwitch.addEventListener('change', function() {
+  isDarkeningEnabled = this.checked;
+});
 
 // Initialize the grid size
 updateGridSize();
